3e57fb57e76cf937643d0226a5c001ce4e3ebf10
pelegOberlender <peleg.oberlender@post.runi.ac.il>
2025-04-20 15:08:58 +0300

    update path

--- tests/utils/path_utils.py
+++ tests/utils/path_utils.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-def find_project_root(marker_folder: str = "home_assignment") -> Path:
+def find_project_root(marker_folder: str = "shopic_home_assignment") -> Path:
     """
     Traverses up from the current file location to find the root of the project.
     """
